9f1c97683ba81e746beebb9ee784e707ff5fe779
Lady-Emporio <godod4@gmail.com>
2018-07-11 21:06:45 +0300

    delete keydown and key up

--- main.cpp
+++ main.cpp
@@ -5,14 +5,33 @@ HHOOK hHook = NULL;
 
 LRESULT CALLBACK MyLowLevelKeyBoardProc(int nCode, WPARAM wParam, LPARAM lParam)
 {
-    if (wParam == WM_KEYDOWN){
-        KBDLLHOOKSTRUCT cKey = *((KBDLLHOOKSTRUCT*)lParam);
-        int key=cKey.vkCode;
+    KBDLLHOOKSTRUCT cKey = *((KBDLLHOOKSTRUCT*)lParam);
+    int key=cKey.vkCode;
+    if (wParam == WM_KEYDOWN ){
         if (key==192){
             emit staticVariable::GetStatic()->w->buttom_Screen->clicked();
         }
+        else if(key==49){//49 =1]
+            QClipboard* c = QApplication::clipboard();
+            c->setText( "Проверка" );
+        }
+        else if(key == 50){ //50=2
+            QDate dateToday = QDate::currentDate();
+            QString now=dateToday.toString("Р dd.MM.yy Л2");
+            QClipboard* c = QApplication::clipboard();
+            c->setText( now );
+        }else{
+            return CallNextHookEx(hHook, nCode, wParam, lParam);
+        }
     }
-    return CallNextHookEx(hHook, nCode, wParam, lParam);
+    else if (wParam ==WM_KEYUP){
+           if (key==192 || key==49 || key == 50){
+
+           }else{
+               return CallNextHookEx(hHook, nCode, wParam, lParam);
+           }
+    }
+
 }
 
 int main(int argc, char *argv[])
